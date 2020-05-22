d4f4695c7674297362e6c382b5e70fc9dc29b23a
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2020-05-22 22:12:18 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,18 @@
+#this is a new python file for the class
+
+'''
+   this is a new python file for the class
+   Bulk comments
+'''
+
+print('hello world!')
+print("welcome to shubham's world!",'hope you see good')
+print('happy learning\n Welcome to python')
+a=10
+B="edureka!"
+print(a,B)
+#another example
+x,y,z=10,20,"shubham"
+print(x,y,z)
+print(y)
+print(z)
